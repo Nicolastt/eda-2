c8bc93a9c872e37d29b54126d462e194d241955c
Nicholas <gianfrancopr08@gmail.com>
2024-07-29 15:39:56 -0500

    Implementación KMP

---
+++ kmp.py
@@ -0,0 +1,44 @@
+def failure_function(P):
+    m = len(P)
+    valor_f = [0] * m
+    k = 0
+    j = 1
+
+    while j < m:
+        if P[j] == P[k]:
+            valor_f[j] = k + 1
+            j += 1
+            k += 1
+        elif k > 0:
+            k = valor_f[k - 1]
+        else:
+            j += 1
+    return valor_f
+
+
+def find_kmp(T, P):
+    n, m = len(T), len(P)
+
+    if m == 0:
+        return 0
+
+    result_failure = failure_function(P)
+    print('f(k) =', result_failure)
+
+    j = 0
+    k = 0
+
+    while j < n:
+        if T[j] == P[k]:
+            if k == m - 1:
+                return j - m + 1
+            j += 1
+            k += 1
+        elif k > 0:
+            k = result_failure[k - 1]
+        else:
+            j += 1
+    return -1
+
+
+find_kmp('acacabacababacacac', 'acacac')
